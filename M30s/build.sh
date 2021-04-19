f7e402e8ad780e5e94a133ada07f457bafd3fedd
neel0210 <neeljadhav0210@gmail.com>
2021-04-20 01:55:50 +0700

    added: build script

---
+++ M30s/build.sh
@@ -0,0 +1,71 @@
+#
+#!/bin/bash
+
+echo "Setting Up Environment"
+echo ""
+export ARCH=arm64
+export SUBARCH=arm64
+export ANDROID_MAJOR_VERSION=r
+export PLATFORM_VERSION=11.0.0
+
+# Export KBUILD flags
+export KBUILD_BUILD_USER=neel0210
+export KBUILD_BUILD_HOST=hell
+
+# CCACHE
+export CCACHE="$(which ccache)"
+export USE_CCACHE=1
+ccache -M 50G
+export CCACHE_COMPRESS=1
+
+# TC LOCAL PATH
+export CROSS_COMPILE=$(pwd)/gcc/bin/aarch64-linux-android-
+export CLANG_TRIPLE=$(pwd)/clang/bin/aarch64-linux-gnu-
+export CC=$(pwd)/clang/bin/clang
+
+# Check if have gcc/32 & clang folder
+if [ ! -d "$(pwd)/gcc/" ]; then
+   git clone --depth 1 git://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 gcc
+fi
+
+if [ ! -d "$(pwd)/clang/" ]; then
+   git clone --depth 1 https://github.com/PrishKernel/toolchains.git -b proton-clang12 clang
+fi
+
+echo "======================="
+echo "Making kernel with ZIP"
+echo "======================="
+make clean && make mrproper
+rm ./arch/arm64/boot/Image
+rm ./arch/arm64/boot/Image.gz
+rm ./Image
+rm ./output/*.zip
+rm ./PRISH/AIK/image-new.img
+rm ./PRISH/AIK/ramdisk-new.cpio.empty
+rm ./PRISH/AIK/split_img/boot.img-zImage
+rm ./PRISH/AK/Image
+rm ./PRISH/ZIP/PRISH/D/M30S/boot.img
+rm ./PRISH/AK/*.zip
+clear
+echo "======================="
+echo "Making kernel with ZIP"
+echo "======================="
+make m30sdd_defconfig
+make -j'nproc'
+echo "Kernel Compiled"
+echo ""
+echo "======================="
+echo "Packing Kernel INTO ZIP"
+echo "======================="
+echo ""
+cp -r ./arch/arm64/boot/Image ./PRISH/AIK/split_img/boot.img-zImage
+cp -r ./arch/arm64/boot/Image ./PRISH/AK/Image
+./PRISH/AIK/repackimg.sh
+cp -r ./PRISH/AIK/image-new.img ./PRISH/ZIP/PRISH/D/M30S/boot.img
+cd PRISH/ZIP
+echo "==========================="
+echo "Packing into Flashable zip"
+echo "==========================="
+./zip.sh
+cd ../..
+cp -r ./PRISH/ZIP/1.zip ./output/Kakarot_Kernel-Enforcing-N1-M30sdd.zip
